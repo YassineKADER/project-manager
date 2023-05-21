cc6e90db949081cea4c1c1ed647478c81313d173
EROS <yassine.kader2017fc@gmail.com>
2023-05-21 22:11:54 +0100

    add the add users and delete users functions

---
+++ project-manager/src/components/items/AddUser.jsx
@@ -0,0 +1,105 @@
+import { arrayUnion, collection, getDocs, doc, updateDoc, getDoc} from "firebase/firestore/lite";
+import { db } from "../../firebase";
+import { useEffect, useState } from "react";
+import { Autocomplete, Button, Card, Modal, Snackbar, TextField, Alert} from "@mui/material";
+
+export const AddUser = ({open, openSet, projectId}) => {
+  const [users, setUsers] = useState([]);
+  const [usersToAdd, setUsersToAdd] = useState([])
+  const [snackBar, setSnackBar] = useState(false)
+  useEffect(() => {
+    try {
+      getDocs(collection(db, "users")).then((queryJob) => {
+        const emails = queryJob.docs.map((doc) => {
+          return doc.id;
+        });
+        // const emails = queryJob.docs.map((doc)=>{doc.id})
+        setUsers(emails);
+      });
+    } catch (e) {
+      console.log("error");
+    }
+  }, []);
+  const filterOptions = (options, state) => {
+    if (state.inputValue.trim() === "") {
+      return [];
+    }
+    return options;
+  };
+  const getOptionDisabled = (option, { inputValue }) => {
+    return inputValue.trim() === "";
+  };
+
+  const handleInputChange = (event, newInputValue) => {
+    setUsersToAdd(newInputValue)
+  };
+
+  const handelAddUserClick = ()=>{
+    getDoc(doc(db, 'projects', projectId))
+  .then((snapshot) => {
+    const usersRefs = snapshot.get('members');
+    const newUsersRefs = usersToAdd.map((email) => doc(db, 'users', email));
+    console.log(usersRefs);
+    console.log(newUsersRefs);
+
+    const flattenedRefs = [...usersRefs, ...newUsersRefs];
+
+    const updatedReferences = arrayUnion(...flattenedRefs);
+
+    return updateDoc(doc(db, 'projects', projectId), { members: updatedReferences });
+  })
+  .then(() => {
+    setSnackBar(true)
+  })
+  .catch(() => {
+    console.log('not cool');
+  });
+
+  }
+  return (
+    <>
+    <Modal
+      open={open}
+      onClose={()=>openSet(false)}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      keepMounted
+      aria-labelledby="keep-mounted-modal-title"
+      aria-describedby="keep-mounted-modal-description"
+    >
+      <Card
+        style={{
+          minWidth: "50%",
+          minHeight: "30%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection:"column"
+        }}
+      >
+        <Autocomplete
+          multiple
+          disablePortal
+          id="combo-box-demo"
+          options={users}
+          style={{minWidth:"500px"}}
+          filterOptions={filterOptions}
+          onChange={handleInputChange}
+          renderInput={(params) => <TextField {...params} label="User Email" />}
+        />
+        <div style={{marginTop:"20px",width:"70%", display:"flex", alignItems:"right", justifyContent:"right"}}><Button variant="outlined" onClick={()=>{handelAddUserClick()}}>Add Users</Button></div>
+      </Card>
+    </Modal>
+    
+    <Snackbar open={snackBar} autoHideDuration={1500} onClose={()=>{setSnackBar(false)}}>
+    <Alert severity="success" sx={{ width: '100%' }}>
+      Users Added !
+    </Alert>
+  </Snackbar>
+  
+    </>
+  );
+};
--- project-manager/src/components/pages/Project.jsx
+++ project-manager/src/components/pages/Project.jsx
@@ -1,32 +1,85 @@
-import { useEffect, useRef, useState } from "react"
-import { UserAuth } from "../context/AuthContext"
-import { Navbar } from "../items/Navbar"
-import {Chart} from "react-google-charts"
-import { Container, Grid } from "@mui/material"
-import { useParams } from "react-router-dom"
+import { useEffect, useRef, useState } from "react";
+import { UserAuth } from "../context/AuthContext";
+import { Navbar } from "../items/Navbar";
+import { Chart } from "react-google-charts";
+import {
+  Container,
+  Grid,
+  Avatar,
+  Chip,
+  Card,
+  Typography,
+  Snackbar,
+  Alert,
+  Button,
+} from "@mui/material";
+import { useParams } from "react-router-dom";
+import {
+  getDoc,
+  doc,
+  getDocs,
+  arrayRemove,
+  updateDoc,
+} from "firebase/firestore/lite";
+import { db } from "../../firebase";
+import DeleteIcon from "@mui/icons-material/Add";
+import { AddUser } from "../items/AddUser";
 
-export const Project  = ()=>{
-    const {uid} = useParams()
-    const {user, getUser} = UserAuth()
-    const [userInfo, setUserInfo] = useState(null)
-    useEffect(()=>{
-        console.log("hello world")
-        console.log(user)
-        
-    },[])
-    const data = [
+export const Project = () => {
+  const { uid } = useParams();
+  const { user, getUser } = UserAuth();
+  const [members, setUsers] = useState([]);
+  const [userInfo, setUserInfo] = useState(null);
+  const [addUser, setAddUser] = useState(false);
+  const [deleteSnackBar, setDeleteSnackBar] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log("hello world");
+        console.log(user);
+        const docRef = doc(db, "projects", uid);
+        const docSnapshot = await getDoc(docRef);
+        const data = docSnapshot.data();
+
+        let leaderRefs = data["leader"];
+        let memberRefs = data["members"];
+
+        // Fetch leader documents concurrently
+        const leaderPromises = leaderRefs.map((ref) => getDoc(ref));
+        const leaderSnapshots = await Promise.all(leaderPromises);
+        const leaders = leaderSnapshots.map((snapshot) => snapshot.id);
+
+        // Fetch member documents concurrently
+        const memberPromises = memberRefs.map((ref) => getDoc(ref));
+        const memberSnapshots = await Promise.all(memberPromises);
+        const members_arr = memberSnapshots.map((snapshot) => ({
+          ref: snapshot.data(),
+          id: snapshot.id,
+          is_leader: leaders.includes(snapshot.id),
+        }));
+
+        setUsers(members_arr);
+        console.log(members_arr);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [addUser]);
+  const data = [
     [
-      { type: 'string', label: 'Task ID' },
-      { type: 'string', label: 'Task Name' },
-      { type: 'date', label: 'Start Date' },
-      { type: 'date', label: 'End Date' },
-      { type: 'number', label: 'Duration' },
-      { type: 'number', label: 'Percent Complete' },
-      { type: 'string', label: 'Dependencies' },
+      { type: "string", label: "Task ID" },
+      { type: "string", label: "Task Name" },
+      { type: "date", label: "Start Date" },
+      { type: "date", label: "End Date" },
+      { type: "number", label: "Duration" },
+      { type: "number", label: "Percent Complete" },
+      { type: "string", label: "Dependencies" },
     ],
     [
-      'Task 1',
-      'Design',
+      "Task 1",
+      "Design",
       new Date(2022, 1, 1),
       new Date(2022, 1, 5),
       null,
@@ -34,50 +87,198 @@ export const Project  = ()=>{
       null,
     ],
     [
-      'Task 2',
-      'Development',
+      "Task 2",
+      "Development",
       new Date(2022, 1, 6),
       new Date(2022, 1, 20),
       null,
       25,
-      'Task 1',
+      "Task 1",
     ],
     [
-      'Task 3',
-      'Testing',
+      "Task 3",
+      "Testing",
       new Date(2022, 1, 21),
       new Date(2022, 2, 1),
       null,
       0,
-      'Task 2, Task 1',
+      "Task 2, Task 1",
+    ],
+    [
+      "Task 3",
+      "Testing",
+      new Date(2022, 1, 21),
+      new Date(2022, 2, 1),
+      null,
+      0,
+      "Task 2, Task 1",
+    ],
+    [
+      "Task 4",
+      "hi",
+      new Date(2022, 1, 21),
+      new Date(2022, 2, 1),
+      null,
+      0,
+      "Task 2, Task 1",
+    ],
+    [
+      "Task 5",
+      "Testing",
+      new Date(2022, 1, 21),
+      new Date(2022, 2, 1),
+      null,
+      0,
+      "Task 2, Task 1",
+    ],
+    [
+      "Task 5",
+      "Testing",
+      new Date(2022, 1, 21),
+      new Date(2022, 2, 31),
+      null,
+      0,
+      "Task 2, Task 1",
     ],
   ];
   function handleChartSelect(selection) {
     // Handle chart selection events
     console.log(selection);
   }
-    return(
-        <div>
-        <Navbar username={"yassine"+" "+"lastName"} profeilPicture={""}></Navbar>
-        <h1 style={{textAlign:"left"}}>Gantt Diagramme</h1>
-        
-        <Grid container style={{height:"100%"}}>
-        <Grid item xs={8} style={{height:"100%"}}>
-        <Chart
-      chartType="Gantt"
-      data={data}
-      width="100%"
-      height="100%"
-      options={{
-        gantt: {
-          trackHeight: 50,
-        },
-      }}
-    /></Grid>
-    <Grid item xs={4} style={{height:"100%"}}><h1>Tasks</h1></Grid>
-        
-    </Grid>
-    {uid}
+
+  function handelMemberDelete(data) {
+    const documentRef = doc(db, "projects", uid);
+    console.log(data);
+    const userToDelete = doc(db, "users", data);
+    const updateObject = {
+      members: arrayRemove(userToDelete),
+    };
+    updateDoc(documentRef, updateObject).then(() => {
+      console.log("user deleted");
+    });
+    setUsers(members.filter((item) => item.id !== data));
+    setDeleteSnackBar(true);
+  }
+
+  const handleAddUserClose = () => {
+    setAddUser(false);
+  };
+
+  return (
+    <div style={{ height: "100%", width: "100%" }}>
+      <Navbar
+        username={"yassine" + " " + "lastName"}
+        profeilPicture={""}
+      ></Navbar>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Card
+          variant={"outlined"}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            rowGap: "10",
+            gap: "5",
+            alignItems: "center",
+            width: "90%",
+            padding: "5px",
+            marginTop: "10px",
+          }}
+        >
+          {
+            <Typography variant="body2" style={{ marginRight: "5px" }}>
+              Members:{" "}
+            </Typography>
+          }
+          {members.map((mem, index) => (
+            <Chip
+              key={mem.id}
+              label={mem.ref.firstName + " " + mem.ref.lastName}
+              avatar={<Avatar src={mem.ref.profeilPhoto}></Avatar>}
+              onDelete={
+                mem.is_leader == true
+                  ? undefined
+                  : (data) => handelMemberDelete((data = mem.id))
+              }
+            ></Chip>
+          ))}
+          <Chip
+            label={"Add"}
+            deleteIcon={<DeleteIcon></DeleteIcon>}
+            onClick={() => {
+              console.log("im clicked");
+              setAddUser(true);
+            }}
+            onDelete={() => {
+              setAddUser(true);
+              console.log("hi");
+            }}
+          ></Chip>
+        </Card>
+
+        <div style={{ width: "90%", marginTop: "2rem", height: "100%" }}>
+          <Grid container style={{ height: "100%" }}>
+            <Grid item xs={8} style={{ height: (10*data.length).toString()+"vh"}}>
+              <Card
+                variant={"outlined"}
+                style={{
+                  height: "50px",
+                  marginBottom: "5px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography variant="body2" style={{ marginLeft: "5px" }}>
+                  Gantt Chart:{" "}
+                </Typography>
+                <div
+                  style={{ marginRight: "5px", display: "flex", gap: "5px" }}
+                >
+                  <Button variant="outlined">Delete</Button>
+                  <Button variant="outlined">Edit</Button>
+                  <Button variant="outlined">Add</Button>
+                </div>
+              </Card>
+              <Chart
+                chartType="Gantt"
+                data={data}
+                width="100%"
+                height="100%"
+                options={{
+                  gantt: {
+                    trackHeight: 50,
+                  },
+                }}
+              />
+            </Grid>
+            <Grid item xs={4} style={{ height: "100%" }}>
+              <h1>Tasks</h1>
+              <h1>Task1</h1>
+            </Grid>
+          </Grid>
         </div>
-    )
-}
+      </div>
+      <AddUser open={addUser} openSet={setAddUser} projectId={uid}></AddUser>
+      {uid}
+      <Snackbar
+        open={deleteSnackBar}
+        autoHideDuration={1500}
+        onClose={() => {
+          setDeleteSnackBar(false);
+        }}
+      >
+        <Alert severity="info" sx={{ width: "100%" }}>
+          User Deleted !
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+};
